5c2696e8a425ee71e149bd7ef46aa3a8b2d6b45a
Thai Son <sonlamngan2015@gmail.com>
2022-01-15 21:55:37 -0700

    added collapasable quiz sections

---
+++ app.js
@@ -0,0 +1,8 @@
+//--------------------------------------------------Home Page-----------------------------------------------//
+function beginButtonClicked(){
+    document.location.href="orientation.html"
+}
+function calculatorButtonClicked(){
+    document.location.href="calculatorPage.html"
+}
+//----------------------------------------------End Of Home Page--------------------------------------------//

5f0978bc57d3ca12c85e5961aa88ceb30dcb83a3
Thaison Nghiem <sonlamngan2015@gmail.com>
2022-01-17 11:15:13 -0700

    added a quiz validator

--- app.js
+++ app.js
@@ -13,23 +13,24 @@ function toWaiverPage(){
 }
 
 function validateAnswer(){
-    var correctAnswers = [1,3,2,3,4]
-    //find selected answer for question i
+    var correctAnswers = [1, 3, 2, 3, 4]
+    var numberOfCorrectAnswers = 0;
+    
     for(var i=1; i<=5; i++){
-        var r = document.querySelector('input[name=q'+i+']:checked');
-    }
-    //answer for question i
-    var solution = correctAnswers[i-1];
-    //if the answer if correct
-    if(r&&r.value == 'r'+solution){
-        //highlight the answer green
-        document.getElementById('q'+i+'_r'+solution).parentNode.style.color='#5cb85c';
-        document.getElementById('q'+i+'_r'+solution).parentNode.style.fontWeight='bold';
-    }
-    //if the answer is not correct 
-    if(r&&r.value != 'r'+solution){
-        document.getElementById('q'+i+'_r'+solution).parentNode.style.color='#FF0000';
-        document.getElementById('q'+i+'_r'+solution).parentNode.style.fontWeight='bold';
-    }
+        //find selected answer for question 
+        var checkedAnswer = document.querySelector('input[name=q'+i+']:checked');
+        //answer for question i
+        var solution = correctAnswers[i-1];
+        //if the answer if correct
+        if(checkedAnswer&&checkedAnswer.value == 'r'+solution){
+            //highlight the answer green
+            document.getElementById('q'+i+'_r'+solution).parentNode.style.color='#5cb85c';
+            document.getElementById('q'+i+'_r'+solution).parentNode.style.fontWeight='bold';
+
+            numberOfCorrectAnswers++;
+        }
+        //if the answer is not correct 
+        
+    }  
 }
 //------------------------------------------End of Quiz Page------------------------------------------------//
